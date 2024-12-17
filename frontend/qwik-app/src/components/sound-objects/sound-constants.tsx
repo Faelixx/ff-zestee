8b7fbdd0d5cb90909cb11caaf8dd2872dfac4db5
faelixx1337 <sasha.taylor7@gmail.com>
2024-12-17 11:44:19 -0700

    Feat: add sound-constants component object

---
+++ frontend/qwik-app/src/components/sound-objects/sound-constants.tsx
@@ -0,0 +1,13 @@
+interface soundConstants {
+    [index: number]: {id: string, key: string, sound: string, title: string}
+}
+
+export const soundConstants = 
+[
+    {
+        id: "",
+        key: "q",
+        sound: "",
+        title: "",
+    },
+] 
