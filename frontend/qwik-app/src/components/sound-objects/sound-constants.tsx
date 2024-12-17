61d7a48f0fee68e15d56a6c0db50bd0fc2b90529
faelixx1337 <sasha.taylor7@gmail.com>
2024-12-17 12:29:32 -0700

    Feat: add sounds to repo

--- frontend/qwik-app/src/components/sound-objects/sound-constants.tsx
+++ frontend/qwik-app/src/components/sound-objects/sound-constants.tsx
@@ -5,9 +5,100 @@ interface soundConstants {
 export const soundConstants = 
 [
     {
-        id: "",
-        key: "q",
-        sound: "",
-        title: "",
+        id: "1",
+        key: "1",
+        title: "Conveyer_EM",
+        sound: "../../../public/sounds/1-conveyer_Em.wav",
     },
+    {
+        id: "2",
+        key: "2",
+        title: "Triad_Em",
+        sound: "../../../public/sounds/2-triad_Em.wav",
+    },
+    {
+        id: "3",
+        key: "3",
+        title: "Reflections_G#m",
+        sound: "../../../public/sounds/3-reflextions_G#m.wav",
+    },
+    {
+        id: "4",
+        key: "4",
+        title: "Ring_G#m",
+        sound: "../../../public/sounds/4-ring_G#m.wav",
+    },
+    {
+        id: "5",
+        key: "q",
+        title: "SFX",
+        sound: "../../../public/sounds/q-SFX.wav",
+    },
+    {
+        id: "6",
+        key: "w",
+        title: "FX Alarm",
+        sound: "../../../public/sounds/w-FX Alarm.wav",
+    },
+    {
+        id: "7",
+        key: "e",
+        title: "Hey!",
+        sound: "../../../public/sounds/e-Hey.wav",
+    },
+    {
+        id: "8",
+        key: "r",
+        title: "Chop",
+        sound: "../../../public/sounds/r-CHOP.wav",
+    },
+    {
+        id: "9",
+        key: "a",
+        title: "808",
+        sound: "../../../public/sounds/a-808.wav",
+    },
+    {
+        id: "10",
+        key: "s",
+        title: "Clap",
+        sound: "../../../public/sounds/s-Clap.wav",
+    },
+    {
+        id: "11",
+        key: "d",
+        title: "Triangle",
+        sound: "../../../public/sounds/d-Triangle.wav",
+    },
+    {
+        id: "12",
+        key: "f",
+        title: "Crash",
+        sound: "../../../public/sounds/f-Crash.wav",
+    },
+    {
+        id: "13",
+        key: "z",
+        title: "Kick",
+        sound: "../../../public/sounds/z-Kick.wav",
+    },
+    {
+        id: "14",
+        key: "x",
+        title: "Snare",
+        sound: "../../../public/sounds/x-Snare.wav",
+    },
+    {
+        id: "15",
+        key: "c",
+        title: "HH",
+        sound: "../../../public/sounds/c-HH.wav",
+    },
+    {
+        id: "16",
+        key: "v",
+        title: "OH",
+        sound: "../../../public/sounds/v-OH.wav",
+    },
+    
 ] 
